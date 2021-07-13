11f5911872825d2d7ae6b46fa62a1fa51367e284
Akshay Sanganal <akshaysanganal@gmail.com>
2021-07-13 15:53:54 -0400

    added docker implementation ... also fixed some ports definition for the microservices

--- src/ApiGatways/Shopping.Aggregator/Services/OrderService.cs
+++ src/ApiGatways/Shopping.Aggregator/Services/OrderService.cs
@@ -11,10 +11,12 @@ namespace Shopping.Aggregator.Services
     public class OrderService : IOrderService
     {
         private readonly HttpClient _client;
+
         public OrderService(HttpClient client)
         {
             _client = client ?? throw new ArgumentNullException(nameof(client));
         }
+
         public async Task<IEnumerable<OrderResponseModel>> GetOrdersByUserName(string userName)
         {
             var response = await _client.GetAsync($"/api/v1/Order/{userName}");
